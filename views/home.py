dad55de340d7bcecddfe9838c4a533b35e5f9382
lukaszziobro89 <lukasz.ziobro.89@gmail.com>
2021-12-03 21:58:41 +0100

    some progress

--- views/home.py
+++ views/home.py
@@ -6,12 +6,20 @@ router = fastapi.APIRouter()
 
 @router.get("/")
 @template(template_file='home/index.html')
-def index(user: str = 'anonymous'):
+def index():
     return {
-        'user_name': user
+        'package_count': 274_000,
+        'release_count': 2_234_847,
+        'user_count': 73_874,
+        'packages': [
+            {'id': 'fastapi', 'summary': "A great web framework"},
+            {'id': 'uvicorn', 'summary': "Your favorite ASGI server"},
+            {'id': 'httpx', 'summary': "Requests for an async world"},
+        ]
     }
 
 
 @router.get("/about")
+@template(template_file='home/about.html')
 def about():
     return {}
