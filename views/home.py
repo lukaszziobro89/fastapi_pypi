edd4dbe12cf69e4ab458165dba109759ba27b51f
lukaszziobro89 <lukasz.ziobro.89@gmail.com>
2021-12-04 18:05:56 +0100

    standardize names

--- views/home.py
+++ views/home.py
@@ -2,7 +2,7 @@ import fastapi
 from fastapi_chameleon import template
 from starlette.requests import Request
 
-from viewmodels.home.indexviewmodel import IndexViewModel
+from viewmodels.home.index_viewmodel import IndexViewModel
 from viewmodels.shared.viewmodel import ViewModelBase
 
 router = fastapi.APIRouter()
