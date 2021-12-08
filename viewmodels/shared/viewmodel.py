daa7d1451d96f2cd3363669302a1ef6020e1549b
lukaszziobro89 <lukasz.ziobro.89@gmail.com>
2021-12-08 20:38:07 +0100

    set cookies

--- viewmodels/shared/viewmodel.py
+++ viewmodels/shared/viewmodel.py
@@ -2,6 +2,8 @@ from typing import Optional
 
 from starlette.requests import Request
 
+from infrastructure import cookie_auth
+
 
 class ViewModelBase:
 
@@ -9,7 +11,7 @@ class ViewModelBase:
         self.request: Request = request
         self.error: Optional[str] = None
         self.user_id: Optional[int] = None
-        self.is_logged_in = False
+        self.is_logged_in = cookie_auth.get_user_id_from_cookie(self.request)
 
     def to_dict(self) -> dict:
         return self.__dict__
