d7f3235b2ab9353e6e77993ea1aa65da993edbca
lukaszziobro89 <lukasz.ziobro.89@gmail.com>
2021-12-08 19:36:09 +0100

    register form

--- viewmodels/account/login_viewmodel.py
+++ viewmodels/account/login_viewmodel.py
@@ -1,5 +1,12 @@
+from typing import Optional
+
+from starlette.requests import Request
+
 from viewmodels.shared.viewmodel import ViewModelBase
 
 
 class LoginViewModel(ViewModelBase):
-    pass
+    def __init__(self, request: Request):
+        super().__init__(request)
+        self.email: Optional[str] = None
+        self.password: Optional[str] = None
--- views/account.py
+++ views/account.py
@@ -10,18 +10,34 @@ router = fastapi.APIRouter()
 
 
 @router.get("/account")
+@template(template_file="account/index.html")
 def index(request: Request):
     vm = AccountViewModel(request)
     return vm.to_dict()
 
 
 @router.get("/account/register")
+@template(template_file="account/register.html")
 def register(request: Request):
     vm = RegisterViewModel(request)
     return vm.to_dict()
 
 
+@router.post("/account/register")
+@template(template_file="account/register.html")
+async def register(request: Request):
+    vm = RegisterViewModel(request)
+    await vm.load()
+
+    if vm.error:
+        return vm.to_dict()
+
+    print("TODO: REDIRECT")
+    return vm.to_dict()
+
+
 @router.get("/account/login")
+@template(template_file="account/login.html")
 def login(request: Request):
     vm = LoginViewModel(request)
     return vm.to_dict()
