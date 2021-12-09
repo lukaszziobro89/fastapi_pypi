085d46c53a659fe40bc663d7cc47292bb2600232
lukaszziobro89 <lukasz.ziobro.89@gmail.com>
2021-12-09 13:38:58 +0100

    login page

--- viewmodels/account/login_viewmodel.py
+++ viewmodels/account/login_viewmodel.py
@@ -1,5 +1,6 @@
 from typing import Optional
 
+import validators
 from starlette.requests import Request
 
 from viewmodels.shared.viewmodel import ViewModelBase
@@ -10,3 +11,15 @@ class LoginViewModel(ViewModelBase):
         super().__init__(request)
         self.email: Optional[str] = None
         self.password: Optional[str] = None
+
+    async def load(self):
+        form = await self.request.form()
+        self.email = form.get('email')
+        self.password = form.get('password')
+
+        if not self.email or not self.email.strip():
+            self.error = "Email is required"
+        elif not validators.email(self.email):
+            self.error = "Not a valid email"
+        if not self.password or not self.password.strip():
+            self.error = "Password is required"
--- views/account.py
+++ views/account.py
@@ -51,6 +51,26 @@ def login(request: Request):
     return vm.to_dict()
 
 
+@router.post("/account/login")
+@template(template_file="account/login.html")
+async def login(request: Request):
+    vm = LoginViewModel(request)
+    await vm.load()
+
+    if vm.error:
+        return vm.to_dict()
+
+    user = user_service.login_user(vm.email, vm.password)
+    if not user:
+        vm.error = "The account does not exists or the password is wrong"
+        return vm.to_dict()
+
+    resp = fastapi.responses.RedirectResponse('/account', status_code=status.HTTP_302_FOUND)
+    cookie_auth.set_auth(resp, user.id)
+
+    return resp
+
+
 @router.get("/account/logout")
 def logout():
     # Logout user
