4ddcf0cc53b70b48665e341844bd9ced319ca1e9
lukaszziobro89 <lukasz.ziobro.89@gmail.com>
2021-12-09 13:56:36 +0100

    small change in login

--- viewmodels/account/login_viewmodel.py
+++ viewmodels/account/login_viewmodel.py
@@ -9,13 +9,13 @@ from viewmodels.shared.viewmodel import ViewModelBase
 class LoginViewModel(ViewModelBase):
     def __init__(self, request: Request):
         super().__init__(request)
-        self.email: Optional[str] = None
-        self.password: Optional[str] = None
+        self.email = ''
+        self.password = ''
 
     async def load(self):
         form = await self.request.form()
-        self.email = form.get('email')
-        self.password = form.get('password')
+        self.email = form.get('email').lower().strip()
+        self.password = form.get('password').strip()
 
         if not self.email or not self.email.strip():
             self.error = "Email is required"
