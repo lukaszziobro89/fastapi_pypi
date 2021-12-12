689914db5305483a99cd67e3bea29b51595b785a
lukaszziobro89 <lukasz.ziobro.89@gmail.com>
2021-12-12 15:31:16 +0100

    package and release sqlalchemy

--- data/package.py
+++ data/package.py
@@ -1,21 +1,39 @@
-class Package:
+import datetime
+from typing import List
 
-    def __init__(self,
-                 package_name: str,
-                 summary: str,
-                 description: str,
-                 home_page: str,
-                 lic: str,
-                 author_name:str,
-                 maintainers: list = None
-                 ):
-        self.id = package_name
-        self.package_name = package_name
-        self.summary = summary
-        self.description = description
-        self.home_page = home_page
-        self.license = lic
-        self.author_name = author_name
-        if maintainers is None:
-            maintainers = []
-        self.maintainers = maintainers
+from data.modelbase import SqlAlchemyBase
+
+import sqlalchemy as sa
+from sqlalchemy import orm
+
+from data.release import Release
+
+
+class Package(SqlAlchemyBase):
+
+    __tablename__ = 'packages'
+
+    id: str = sa.Column(sa.String, primary_key=True)
+    created_date: datetime.datetime = sa.Column(sa.DateTime, default=datetime.datetime.now, index=True)
+    last_updated: datetime.datetime = sa.Column(sa.DateTime, default=datetime.datetime.now, index=True)
+    summary: str = sa.Column(sa.String, nullable=False)
+    description: str = sa.Column(sa.String, nullable=True)
+
+    home_page: str = sa.Column(sa.String)
+    docs_url: str = sa.Column(sa.String)
+    package_url: str = sa.Column(sa.String)
+
+    author_name: str = sa.Column(sa.String)
+    author_email: str = sa.Column(sa.String, index=True)
+
+    license: str = sa.Column(sa.String, index=True)
+
+    # releases relationship
+    releases: List[Release] = orm.relationship("Release", order_by=[
+        Release.major_ver.desc(),
+        Release.minor_ver.desc(),
+        Release.build_ver.desc(),
+    ], back_populates='package')
+
+    def __repr__(self):
+        return '<Package {}>'.format(self.id)
--- data/release.py
+++ data/release.py
@@ -1,8 +1,31 @@
 import datetime
 
+import sqlalchemy as sa
+from sqlalchemy import orm
 
-class Release:
+from data.modelbase import SqlAlchemyBase
 
-    def __init__(self, version: str, created_date: datetime):
-        self.version = version
-        self.created_date = created_date
+
+class Release(SqlAlchemyBase):
+
+    __tablename__ = 'releases'
+
+    id: int = sa.Column(sa.Integer, primary_key=True, autoincrement=True)
+
+    major_ver: int = sa.Column(sa.BigInteger, index=True)
+    minor_ver: int = sa.Column(sa.BigInteger, index=True)
+    build_ver: int = sa.Column(sa.BigInteger, index=True)
+
+    created_date: datetime.datetime = sa.Column(sa.DateTime, default=datetime.datetime.now, index=True)
+
+    comment: str = sa.Column(sa.String)
+    url: str = sa.Column(sa.String)
+    size: int = sa.Column(sa.BigInteger)
+
+    # Package relationship
+    package_id: str = sa.Column(sa.String, sa.ForeignKey("packages.id"))
+    package = orm.relationship('Package')
+
+    @property
+    def version_text(self):
+        return '{}.{}.{}'.format(self.major_ver, self.minor_ver, self.build_ver)
